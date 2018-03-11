52893cb7a315b604cc0b7e959e8d3ed7cc761f4a
saipraveench <mailsaipraveen@gmail.com>
2018-03-11 17:07:52 -0400

    Dropping the columns that are having only one value

--- code.R
+++ code.R
@@ -2,3 +2,11 @@
 data <- read_csv("C:/Users/Sai Praveen/Desktop/report/Exercise GoodHealthCo-20160921/GoodHealthCo-20160921/SBU_example_Surveydata_2014.csv")
 
 #Data Cleaning
+colnames(data)
+
+#Dropping the columns that are having only one value
+
+data <- subset(data, select = - c(STATE,DENSTR2,WEIGHT2,HEIGHT3,CTYCODE1,ZIPCODE,HTIN4,HTM4,WTKG3))
+
+#Dropping the columns that have general inforamation and not related to the diabetes
+
