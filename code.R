81ff5ec05c43d57c35cc0331f56e00a6a92dd7b2
saipraveench <mailsaipraveen@gmail.com>
2018-03-11 18:01:07 -0400

    checking for null values

--- code.R
+++ code.R
@@ -1,22 +1,30 @@
 #Loading data
-data <- read_csv("C:/Users/Sai Praveen/Desktop/report/Exercise GoodHealthCo-20160921/GoodHealthCo-20160921/SBU_example_Surveydata_2014.csv")
+ghdata <- read_csv("C:/Users/Sai Praveen/Desktop/report/Exercise GoodHealthCo-20160921/GoodHealthCo-20160921/SBU_example_Surveydata_2014.csv")
 
 #Data Cleaning
-colnames(data)
+colnames(ghdata)
 
 #Dropping the columns that are having only one value
 
-data <- subset(data, select = - c(STATE,DENSTR2,WEIGHT2,HEIGHT3,CTYCODE1,ZIPCODE,HTIN4,HTM4,WTKG3))
+ghdata <- subset(ghdata, select = - c(STATE,DENSTR2,WEIGHT2,HEIGHT3,CTYCODE1,ZIPCODE,HTIN4,HTM4,WTKG3))
 
 #Dropping the columns that have general inforamation and not related to diabetes
 
-data <- subset(data, select = - c(REPNUM, REPDEPTH,FMONTH,IDATE,IMONTH,IDAY,IYEAR,PSU,NATTMPTS,NRECSEL,NRECSTR))
+ghdata <- subset(ghdata, select = - c(REPNUM, REPDEPTH,FMONTH,IDATE,IMONTH,IDAY,IYEAR,PSU,NATTMPTS,NRECSEL,NRECSTR))
 
 #checking for null values
 
-colSums(is.na(data))
+colSums(is.na(ghdata))
+
+#Dropping column LADULT since 99% values are missing
+
+ghdata <- subset(ghdata, select = - c(LADULT))
+
 
 #For STATERES Column, changing the null values to 0 (Assuming blanks as "Non State residents")
 
-table(data$STATERES, exclude = NULL)
+table(ghdata$STATERES, exclude = NULL)
+
+ghdata$STATERES[is.na(data$STATERES)] <- 0
+
 
